d272bfeb54a7cd568e407394f56247fe265a1bb0
rene <renegmed@yahoo.com>
2018-11-28 16:57:39 -0500

    modified for rinkeby deployment.

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const compiledFactory = require('../build/CampaignFactory.json');
+const compiledFactory = require('../build/contracts/CampaignFactory.json');
 
 // const provider = new HDWalletProvider(
 //   'call glow acoustic vintage front ring trade assist shuffle mimic volume reject',
@@ -12,6 +12,12 @@ const provider = new HDWalletProvider(
   'vintage color congress they kangaroo crisp awesome science fury pear hub want',
   'https://rinkeby.infura.io/v3/79a18556aa274a0a9a5ee7304da13e34'
 );
+
+
+// const provider = new HDWalletProvider(
+//   'vocal economy record broccoli traffic still slender cactus misery bless pill slow',
+//   '0x976368105a739f700cb9fb665008a826ac901d94'
+// );
 
 const web3 = new Web3(provider);
 
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -5,6 +5,7 @@ let web3;
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   // We are in the browser and metamask is running.
   web3 = new Web3(window.web3.currentProvider);
+  console.log("++++++++ metamask web3 provider is used ++++++++");
 } else {
   // We are on the server *OR* the user is not running metamask
   // const provider = new Web3.providers.HttpProvider(
@@ -14,6 +15,8 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
     //'https://rinkeby.infura.io/79a18556aa274a0a9a5ee7304da13e34'
     'https://rinkeby.infura.io/v3/79a18556aa274a0a9a5ee7304da13e34'
   );
+
+  console.log("++++++++ NON metamask web3 provider is used ++++++++");
   web3 = new Web3(provider);
 }
 
